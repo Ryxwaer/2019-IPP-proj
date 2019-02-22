d4640969c51b141790d5f83c4329bb786754dab0
jsenlas <xsenca00@stud.fit.vutbr.cz>
2019-02-22 17:52:32 +0100

    sidebars

---
+++ interpret/Errors.py
@@ -0,0 +1,17 @@
+# Error definition
+class Errors:
+    missing_param = 10
+    open_input_file_failed = 11
+    open_output_file_failed = 12
+
+    xml_wrong_format_ = 31
+    xml_lex_or_sem_err = 32
+
+    internal_error = 99
+
+    def __init__(self):
+
+zaruka
+baterka
+testy
+zlava
